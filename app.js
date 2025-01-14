66546704a8cd1aac442ae28848e428b5e13a956f
yeftakun <yeftakun34@gmail.com>
2025-01-14 14:09:14 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const fs = require('fs');
+const port = 3000;
+
+const renderHTML = (path, res) => {
+    fs.readFile(path, (err, data) => {
+        if(err){
+            res.writeHead(404);
+            res.writeHead('Error: file not found');
+        }else{
+            res.write(data);
+        }
+        res.end();
+    });
+};
+
+http.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/html',
+    });
+
+    const url = req.url;
+    
+    if(url === '/about'){
+        renderHTML('./about.html', res);
+    } else if(url === '/contact'){
+        renderHTML('./contact.html', res);
+    } else {
+        renderHTML('./index.html', res);
+    }
+
+})
+.listen(port, () => {
+    console.log(`Server is listening on port ${port}...`);
+})
